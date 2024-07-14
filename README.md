a2fdcf7e3f39e7196e170b01fde72f72c36b2064
jalzae <jalzae@gmail.com>
2024-07-14 12:22:01 +0700

    update git

---
+++ README.md
@@ -0,0 +1,7 @@
+just ordinary debug mode for javascript project
+
+import then use log function on your project
+
+its automatically detect dotenv
+
+and using NODE_ENV on your environment
---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+function log(...args) {
+  const newDate = new Date().toLocaleString()
+  const nodeEnv = process.env.NODE_ENV || 'development';
+  console.log(process.env.NODE_ENV)
+  if (nodeEnv == 'development') console.log(`log ${newDate} :`, ...args)
+}
+
+log('test')
+
+
+module.exports = {
+  log
+}
