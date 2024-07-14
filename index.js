5b938e7473e79e375c02c0d2d09419d4b3207a4d
jalzae <jalzae@gmail.com>
2024-07-14 12:33:55 +0700

    add production note log

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 function log(...args) {
   const newDate = new Date().toLocaleString()
   const nodeEnv = process.env.NODE_ENV || 'development';
-  console.log(process.env.NODE_ENV)
   if (nodeEnv == 'development') console.log(`log ${newDate} :`, ...args)
+}
+
+function note(...args) {
+  const newDate = new Date().toLocaleString()
+  const nodeEnv = process.env.NODE_ENV || 'development';
+  if (nodeEnv == 'production') console.log(`log ${newDate} :`, ...args)
 }
 
 
 module.exports = {
-  log
+  log, note
 }
